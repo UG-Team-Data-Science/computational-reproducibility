a7ed683803448a8774eb0ca5e8397a3efb5dbd06
Venustiano <v.soancatl.aguilar@rug.nl>
2025-12-09 16:24:22 +0000

    Quarto slides

---
+++ compose/extensions.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/with-contenv bash
+
+/usr/local/bin/install-extension GitHub.copilot
+/usr/local/bin/install-extension GitHub.copilot-chat
+/usr/local/bin/install-extension quarto.quarto
+/usr/local/bin/install-extension ms-python.python
+/usr/local/bin/install-extension ms-python.vscode-pylance
